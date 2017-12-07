345895eece365e198998fbf8c858c45093c3fdf5
swaranlata <swaran.lata@imarkinfotech.com>
2017-12-07 10:34:13 +0530

    data added

--- data.php
+++ data.php
@@ -6,7 +6,7 @@ add_filter( 'screen_options_show_screen', '__return_false' );
     add_action( 'manage_product_posts_custom_column', 'wpso23858236_product_column_offercode', 10, 2 );
     function wpso23858236_product_column_offercode( $column, $postid ) {
         if ( $column == 'price' ) {
-            echo '<span class=""><i class="fa fa-pencil"></i></span>';
+            echo '&nbsp;&nbsp;<span class="edit_price" data-id="'.$postid.'">Edit</span>';
         }
     }
 ?>
