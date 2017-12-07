c9fe3b61dc3c1d2589dbec88f7943c82bf4be41a
swaranlata <swaran.lata@imarkinfotech.com>
2017-12-07 19:02:21 +0530

    updated

--- data.php
+++ data.php
@@ -9,4 +9,33 @@ add_filter( 'screen_options_show_screen', '__return_false' );
             echo '&nbsp;&nbsp;<span class="edit_price" data-id="'.$postid.'">Edit</span>';
         }
     }
+
+
+
+
+
+
+
+
+
+/* Access the value outside the tag*/
+$(document).ready(function(){
+    var a=$(".a").contents().filter(function(){
+        return this.nodeType == 3;
+    }).text();
+    alert(a);
+});
+http://jsfiddle.net/euvKK/
+
+
+
+
+/* Create Zip viw putty */
+ zip -r yourcab.zip /var/www/html/yourcab
+/* Git commands */
+     git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/swaranlata/yourcab.git
+git push -u origin master
 ?>
